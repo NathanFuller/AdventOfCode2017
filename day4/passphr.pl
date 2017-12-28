cc0f8017d37f2faf18dc64e21d731e01b893c49e
NathanFuller <nathan.fuller@att.net>
2017-12-27 17:02:35 -0700

    Day 4 part 1 was way too easy.

---
+++ day4/passphr.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+open(my $fin, "<", "/Users/nathanfuller/adventOfCode/day4/list.txt")
+	|| die "$!";
+
+my $total = 0;
+while (my $line = <$fin>){
+	my $isValid = 1;
+	my @words = (split /\s/, $line);
+	@words = sort @words;
+	foreach (0..$#words-1){
+		if ($words[$_] eq $words[$_+1]){
+			$isValid = 0;
+		}	
+	}
+	$total += $isValid;
+
+}
+
+print "Total: $total\n";
