49d2a6c24278fd64d7c89882ecd600f05ac4c987
NathanFuller <nathan.fuller@att.net>
2017-12-28 21:02:32 -0700

    findInList function works now

--- day6/memReallocate.pl
+++ day6/memReallocate.pl
@@ -4,7 +4,7 @@ use warnings;
 
 my @banks = (0,2,7,0);
 
-sub findWhereMax { #takes a list, returns the index of the maximum element.
+sub findWhereMax {	#takes a list, returns the index of the maximum element.
 	my $index = 0;
 	foreach my $check (1..$#_){
 		if ($_[$check] > $_[$index]){
@@ -14,5 +14,17 @@ sub findWhereMax { #takes a list, returns the index of the maximum element.
 	return $index;
 }
 
-print findWhereMax @banks;
+sub findInList {	#give it a target and a list. Returns 1 if target is in list and 0 if not.
+	my $target = shift;
+	foreach my $str (@_){
+		return 1 if ($str eq $target);
+	}
+	return 0;
+}
 
+my $continue = 1;
+my $stepCounter = 0;
+
+print "The string \"3\" is ";
+print "not " unless findInList("3", @banks);
+print "in the list.\n";
