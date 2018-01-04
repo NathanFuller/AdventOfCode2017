2c509ea6cef17706bad0b569766d7bca68898615
NathanFuller <nathan.fuller@att.net>
2018-01-03 21:03:38 -0700

    Figured it out. Day 9 part 1 complete.

--- day9/breaker.pl
+++ day9/breaker.pl
@@ -15,3 +15,18 @@ print "After step 1:\t$stream\n";
 
 $stream =~ s/<[^>]*>//g;
 print "After step 2:\t$stream\n";
+
+my @chars = split //, $stream;
+my $total = 0;
+my $value = 0;
+foreach (@chars){
+	if ($_ eq "{"){
+		$value++;
+		$total += $value;
+	}	
+	if ($_ eq "}"){
+		$value--;
+	}
+}
+
+print "Total score:\t$total\n";
