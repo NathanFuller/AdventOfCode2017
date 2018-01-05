2b49021696e4ef80c15476210ba46ebce4a797ce
NathanFuller <nathan.fuller@att.net>
2018-01-04 17:04:26 -0700

    day 9 part 2 complete.

--- day9/garbageCounter.pl
+++ day9/garbageCounter.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-open (my $fin, "<", "/Users/nathanfuller/adventOfCode/day9/test.txt")
+open (my $fin, "<", "/Users/nathanfuller/adventOfCode/day9/info.txt")
 	|| die "$!";
 
 
@@ -14,9 +14,12 @@ $stream =~ s/!\S//g;
 print "After step 1:\t$stream\n";
 
 my @garbage = $stream =~ /(<[^>]*>)/g;
+my $count = 0;
 foreach (@garbage){
 	my @chars = split //, $_;
 	my $nchars = @chars - 2;
 	print "$_\t$nchars\n";
+	$count += $nchars;
 }
 
+print "Garbage counted: $count\n";
