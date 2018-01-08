7ebbbfdb1bd21a138ecfb91c002853b406b582fc
NathanFuller <nathan.fuller@att.net>
2018-01-07 18:55:22 -0700

    Put getSlice into knots.pl and wrote a little loop to use it

--- day10/knots.pl
+++ day10/knots.pl
@@ -23,6 +23,42 @@ foreach (@lengths){
 my $currentpos = 0;
 my $skipsize = 0;
 
-foreach my $len (@lengths){
+sub getSlice {
+	#I want this to return the proper slice given the current position and the length.
+	#Wrapping around to the front should happen here.
+
+	my ($sliceSize, $curpos, @list) = @_;
+	#Don't worry - perl won't change these references.
+	my @sliceEls;
+	my $count = 0;
+
+	while ($curpos <= $#list and $count < $sliceSize){
+		#put curpos into sliceEls and increment count
+		$sliceEls[$count] = $curpos;
+		$curpos++;
+		$count++;
+	}
+	#reset curpos to 0
+	$curpos = 0;
+	while ($count < $sliceSize){
+		#fill in rest of sliceEls
+		$sliceEls[$count] = $curpos;
+		$curpos++;
+		$count++;
+	}
 	
+	return @sliceEls;
+}
+
+
+
+foreach my $len (@lengths){
+	my @slice = getSlice($len, $currentpos, @list);
+	@list[@slice] = reverse (@list[@slice]);
+	foreach my $thing (@list){
+		print "$thing\t";
+	}
+	print "\n";
+	#move currentpos by $len + $skipsize
+	$skipsize++;
 }
